96d4e5e79007aa359e4198f1543f2f3853daff6d
ttoocs <ttoocs@gmail.com>
2017-03-13 22:00:22 -0600

    Woo, stuff being fixed.

--- scripts/gen_player_char.sh
+++ scripts/gen_player_char.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-BASE=`./gen_frenime.sh | grep -v FRENEMY`
 
-STATS=`./dice 8 9 2`
 
 echo "##APPLY THESE MANNUALLY##"
 echo "STATS ROLL:"
-echo $STATS
+./dice.sh 8 9 2
 echo ""
 echo "INT: "
 echo "REF: "
@@ -24,8 +22,8 @@ echo "LIFT:  (Is BODY * 40)"
 echo "SAVE:  (Is BODY)"
 echo "BT:  (If BODY <= 2 : Very Weak, 3-4: Weak, 5-7:Average, 8-9: Strong, 10: Very Strong"
 echo "BTM:  (If BT Very Weak: 0, Weak: -1, Average : -2, Strong: -3, Very Strong: -4, Superhuman: -5.)"
+echo ""
+echo ""
+./gen_family_background.sh
+./gen_frenime.sh | grep -v FRENEMY
 
-FAM=`./gen_family_background.sh`
-echo $FAM
-
-echo $BASE
